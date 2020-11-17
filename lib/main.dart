a4f2d6280b6778903ee84382f279b29e2d309c28
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-18 00:52:33 +0600

    Task completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue,
+        ),
+        body: AskMeAnything(),
+        backgroundColor: Colors.blue.shade500,
+      ),
+    ),
+  );
+}
+
+class AskMeAnything extends StatefulWidget {
+  @override
+  _AskMeAnythingState createState() => _AskMeAnythingState();
+}
+
+class _AskMeAnythingState extends State<AskMeAnything> {
+  int index = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(
+            () {
+              index = Random().nextInt(5) + 1;
+            },
+          );
+        },
+        child: Image.asset('images/ball$index.png'),
       ),
     );
+  }
+}
